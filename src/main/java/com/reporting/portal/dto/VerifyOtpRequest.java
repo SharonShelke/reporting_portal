d7c0484dfec842acee4ce5b218900859b4468e3c
sharonshelke1 <sharonshelke1@gmail.com>
2026-04-30 19:39:33 +0530

    new changes

--- src/main/java/com/reporting/portal/dto/VerifyOtpRequest.java
+++ src/main/java/com/reporting/portal/dto/VerifyOtpRequest.java
@@ -1,9 +1,11 @@
 package com.reporting.portal.dto;
 
-import lombok.Data;
-
-@Data
 public class VerifyOtpRequest {
     private String email;
     private String otp;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getOtp() { return otp; }
+    public void setOtp(String otp) { this.otp = otp; }
 }
